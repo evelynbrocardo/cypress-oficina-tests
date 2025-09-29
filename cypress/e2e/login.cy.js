d010d4ce05dc8e5b655cc834e166b1bbf7bdb67e
Evelyn Cristina Brocardo <evelynbrocardo@hotmail.com>
2025-09-29 11:59:22 -0300

    modified:   cypress/e2e/login.cy.js 	new file:   "cypress/screenshots/login.cy.js/Testes de Login -- deve fazer login com credenciais v\303\241lidas e redirecionar (failed).png"

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,3 +4,22 @@ describe('Login Oficina Integrada', () => {
     cy.title().should('include', 'Oficina Integrada')
   })
 })
+
+describe('Testes de Login', () => {
+  it('deve fazer login com credenciais válidas e redirecionar', () => {
+    // 1. Visitar a página de login
+    cy.visit('https://sistema.oficinaintegrada.com.br/default.asp'); // Substitua '/login' pela URL da sua página de login
+
+    // 2. Preencher os campos de email e senha
+    cy.get('input[name="Chave"]').type('28039037136'); // Substitua o seletor e o email
+    cy.get('input[name="Usuário"]').type('evelyn'); // Substitua o seletor e o email
+    cy.get('input[name="Senha"]').type('841884'); // Substitua o seletor e a senha
+
+    // 3. Clicar no botão de login
+    cy.get('button[type="login"]').click(); // Substitua o seletor
+
+    // 4. Verificar se o usuário é redirecionado para o dashboard
+    cy.url().should('include', '/dashboard'); // Verifique se a URL contém o caminho esperado
+    cy.get('h1').should('contain', 'Bem-vindo'); // Verifique um elemento na página do dashboard
+  });
+});
