6b98f06257339e3a6eb32acda8caec982e1bda7f
Evelyn <evelynbrocardo@hotmail.com>
2025-09-30 08:24:42 -0300

    Salvando tudo

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // aqui você pode configurar plugins ou eventos
     },
-  },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
 });
