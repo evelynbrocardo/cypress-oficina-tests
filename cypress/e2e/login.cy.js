bf34f3714fcf3d072dd84c24713044323a15960a
Evelyn <evelynbrocardo@hotmail.com>
2025-09-29 21:08:45 -0300

    Nova versão, com dois testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,9 @@
-/*describe('Login Oficina Integrada', () => {
+describe('Abertura do site Oficina Integrada', () => {
   it('Deve acessar a página de login e verificar título', () => {
     cy.visit('https://sistema.oficinaintegrada.com.br/default.asp')
     cy.title().should('include', 'Oficina Integrada')
   })
-})*/
+})
 
 
 describe('Login Oficina Integrada', () => {
